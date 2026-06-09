ee25e0f2ff130b9ca7446399fda183ccd048031f
Shomoita Jahid Mitin <53936503+shomoitamitin@users.noreply.github.com>
2026-06-09 12:21:09 -0500

    Update seed_data.sql
    RL table insert

--- database/seed_data.sql
+++ database/seed_data.sql
@@ -1,1 +1,17 @@
-
+-- Relationship: 
+INSERT INTO relationships (id, relationship_name) VALUES
+(1, 'Spouse'),
+(2, 'Sibling'),
+(3, 'Parent'),
+(4, 'Friend'),
+(5, 'Mother'),
+(6, 'Partner'),
+(7, 'Child'),
+(8, 'Guardian'),
+(9, 'Aunt'),
+(10, 'Uncle'),
+(11, 'Cousin'),
+(12, 'Grandparent'),
+(13, 'Grandchild'),
+(14, 'Legal Representative'),
+(15, 'Executor');
