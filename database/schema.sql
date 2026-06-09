6dc4c818c6810ca7c3ce5fd7ea02489faa6c2759
Shomoita Jahid Mitin <53936503+shomoitamitin@users.noreply.github.com>
2026-06-09 12:14:04 -0500

    Update schema.sql
    Donor Table creation

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,21 @@
 -- SQLite schema will be added here
+-- Donor Table 
+CREATE TABLE donors (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    title TEXT,
+    dob DATE,
+    dod DATE,
+    cause_of_death TEXT,
+    gender TEXT,
+    occupation TEXT,
+    city TEXT,
+    state TEXT,
+    phone TEXT,
+    email TEXT,
+    initial_contact DATE,
+    studies TEXT,
+    veteran BOOLEAN,
+    tag_number INTEGER
+);
